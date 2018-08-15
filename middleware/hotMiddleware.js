e8d4a137d3c1895bb151e47d2c3b632e33ec115c
Dec-F <dec_fan@icloud.com>
2018-08-15 17:29:27 +0800

    up version and fix res headers

--- middleware/hotMiddleware.js
+++ middleware/hotMiddleware.js
@@ -1,17 +1,23 @@
-import hotMiddleware from 'webpack-hot-middleware'
-import { PassThrough } from 'stream'
+import hotMiddleware from 'webpack-hot-middleware';
+import { PassThrough } from 'stream';
 
 export default (compiler, opts) => {
-  const expressMiddleware = hotMiddleware(compiler, opts)
+  const expressMiddleware = hotMiddleware(compiler, opts);
   return async (ctx, next) => {
-    let stream = new PassThrough()
-    ctx.body = stream
-    await expressMiddleware(ctx.req, {
-      write: stream.write.bind(stream),
-      writeHead: (status, headers) => {
-        ctx.status = status
-        ctx.set(headers)
-      }
-    }, next)
-  }
-}
+    let stream = new PassThrough();
+    if (ctx.req.url == '/__webpack_hmr') {
+      ctx.body = stream;
+    }
+    await expressMiddleware(
+      ctx.req,
+      {
+        write: stream.write.bind(stream),
+        writeHead: (status, headers) => {
+          ctx.status = status;
+          ctx.set(headers);
+        }
+      },
+      next
+    );
+  };
+};
